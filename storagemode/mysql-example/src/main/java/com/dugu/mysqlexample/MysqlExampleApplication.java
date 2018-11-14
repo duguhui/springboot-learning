4e808a2adcb2d784eab9ec6895aae0b2bb0fa6c5
dugu <huiliu1026@outlook.com>
2018-11-14 16:13:06 +0800

    create module mysql-example

---
+++ storagemode/mysql-example/src/main/java/com/dugu/mysqlexample/MysqlExampleApplication.java
@@ -0,0 +1,12 @@
+package com.dugu.mysqlexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlExampleApplication.class, args);
+    }
+}
---
+++ storagemode/mysql-example/src/test/java/com/dugu/mysqlexample/MysqlExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dugu.mysqlexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MysqlExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
